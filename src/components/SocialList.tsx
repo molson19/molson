9d0b441e5a3ba40ab4de7ea3bcb62d567210d5d8
molson19 <olsonmichael233@gmail.com>
2020-09-25 17:39:33 -0700

    Add social links, add molson author, update config and tags.

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Twitter from "../assets/twitter-alt.svg";
 import GitHub from "../assets/github-alt.svg";
+import LinkedIn from "../assets/linkedin-alt.svg";
+import Email from "../assets/email-alt.svg";
 import config from "../lib/config";
 
 export function SocialList({}) {
@@ -21,6 +23,22 @@ export function SocialList({}) {
         rel="noopener"
       >
         <GitHub width={24} height={24} fill={"#222"} />
+      </a>
+      <a
+        title="LinkedIn"
+        href={`https://www.linkedin.com/in/${config.linkedin_account}`}
+        target="_blank"
+        rel="noopener"
+      >
+        <LinkedIn width={24} height={24} fill={"#222"} />
+      </a>
+      <a
+        title="Email"
+        href={`mailto:${config.email}`}
+        target="_blank"
+        rel="noopener"
+      >
+        <Email width={24} height={24} fill={"#222"} />
       </a>
       <style jsx>{`
         a {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Michael Olson<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@__molson</span>
+          <h2>An engineer's journey through software development<span className="fancy">.</span></h2>
           <SocialList />
         </div>
       </div>
